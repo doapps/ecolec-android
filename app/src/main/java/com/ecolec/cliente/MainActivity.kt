2e3bb39e0ebec9f451fe1ba151480d92a8273b25
Leonardo Cardenas <leocardenas2898@gmail.com>
2019-06-01 15:09:26 -0500

    Finish design login and register

--- app/src/main/java/com/ecolec/cliente/MainActivity.kt
+++ app/src/main/java/com/ecolec/cliente/MainActivity.kt
@@ -1,14 +1,24 @@
 package com.ecolec.cliente
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ecolec.cliente.util.StatusBarUtil
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        startActivity(Intent(this, MapsActivity::class.java))
+        StatusBarUtil.statusBarChange(window, Color.WHITE)
+        registerUser.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+
+        signInUser.setOnClickListener {
+            startActivity(Intent(this, HomeActivity::class.java))
+        }
     }
 }
